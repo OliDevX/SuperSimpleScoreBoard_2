e72dd98f2f36bc9e15d76470cdf047a0460b9c91
OlidevX <armando.ibarraran@gmail.com>
2016-06-09 11:06:14 -0500

    Creating Game, Display and Team classes, plus updating MainActivity class with full functionallity

---
+++ app/src/main/java/com/absolutezerosoftware/supersimplescoreboard_2/Display.java
@@ -0,0 +1,37 @@
+package com.absolutezerosoftware.supersimplescoreboard_2;
+
+/**
+ * Created by Armando on 18/04/2016.
+ */
+public class Display {
+
+    // Declares each digit for the display as Strings
+    public String Digit_1;
+    public String Digit_2;
+    public String Digit_3;
+
+    // Declares Score as the holder variable for each display
+    public int Points;
+
+    public void SetPoints(int points) {
+        Points = points;
+        if (Points < 10) {
+            Digit_1 = Integer.toString(Points);
+            Digit_2 = "0";
+            Digit_3 = "";
+        }
+
+        if ((Points > 9) && (Points < 100)) {
+            Digit_1 = Integer.toString(Points % 10);
+            Digit_2 = Integer.toString((Points - (Points % 10))/10);
+            Digit_3 = "";
+        }
+
+        if ((Points > 99) && (Points <1000)){
+            Digit_1 = Integer.toString((Points % 100)%10);
+            Digit_2 = Integer.toString(((Points % 100) - ((Points % 100)%10))/10);
+            Digit_3 = Integer.toString((Points - (Points % 100))/100);
+        }
+
+    }
+}
--- app/src/main/java/com/absolutezerosoftware/supersimplescoreboard_2/MainActivity.java
+++ app/src/main/java/com/absolutezerosoftware/supersimplescoreboard_2/MainActivity.java
@@ -1,5 +1,4 @@
 package com.absolutezerosoftware.supersimplescoreboard_2;
-
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +7,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+// These last two imports were used for the exit dialog
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Declares the mGame object that will be used during the program
+    public Game mGame = new Game();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +24,65 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Set listeners to home_layout & visit_layout
+        View mLayoutHome = (View) findViewById(R.id.home_layout);
+        View mLayoutVisit = (View) findViewById(R.id.visit_layout);
+
+        mLayoutHome.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Invoques the ScoreChange Method passing "1" for the Home Team and "true" to increase
+                mGame.ScoreChange(1,true);
+
+                // Then asks to refresh the screen
+                RefreshScreen();
+            }
+        });
+
+        mLayoutHome.setOnLongClickListener(new View.OnLongClickListener(){
+            @Override
+            public boolean onLongClick(View v) {
+                // Invoques the ScoreChange Method passing "1" for the Home Team and "false" to decrease
+                mGame.ScoreChange(1,false);
+
+                // Then asks to refresh the screen
+                RefreshScreen();
+
+                return true;
+            }
+        });
+
+
+        mLayoutVisit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View v) {
+                // Invoques the ScoreChange Method passing "2" for the Visit Team and "true" to increase
+                mGame.ScoreChange(2,true);
+
+                // Then asks to refresh the screen
+                RefreshScreen();
+            }
+        });
+
+        mLayoutVisit.setOnLongClickListener(new View.OnLongClickListener(){
+            @Override
+            public boolean onLongClick(View v) {
+                // Invoques the ScoreChange Method passing "2" for the Visit Team and "false" to decrease
+                mGame.ScoreChange(2,false);
+
+                // Then asks to refresh the screen
+                RefreshScreen();
+
+                return true;
+            }
+        });
+
+        // Initializes the displays to "00" "00"
+        mGame.SetScores(0,0,0);
+
+        // Refreshes the displays on screen
+        RefreshScreen();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -49,4 +115,59 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    // Created to save Score states when orientation changes
+    @Override
+    protected void onSaveInstanceState (Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("TEAM1_POINTS",mGame.mTeam1.mPoints);
+        outState.putInt("TEAM2_POINTS",mGame.mTeam2.mPoints);
+    }
+
+    // Created to restore Score states after orientation changes
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mGame.SetScores(savedInstanceState.getInt("TEAM1_POINTS"),savedInstanceState.getInt("TEAM2_POINTS"),0);
+        RefreshScreen();
+    }
+
+    // Created to refresh screen every time score changes
+    public void RefreshScreen (){
+        // Declare Displays to be updated
+        TextView mDisplay1_first_digit = (TextView) findViewById(R.id.home_first_digit_textview);
+        TextView mDisplay1_second_digit = (TextView) findViewById(R.id.home_second_digit_textview);
+        TextView mDisplay1_third_digit = (TextView) findViewById(R.id.home_third_digit_textview);
+
+        TextView mDisplay2_first_digit = (TextView) findViewById(R.id.visit_first_digit_textview);
+        TextView mDisplay2_second_digit = (TextView) findViewById(R.id.visit_second_digit_textview);
+        TextView mDisplay2_third_digit = (TextView) findViewById(R.id.visit_third_digit_textview);
+
+        // TODO: Declare and code for Set Display on the RefeshScreen method
+
+        mDisplay1_first_digit.setText(mGame.mDisp1_digit1);
+        mDisplay1_second_digit.setText(mGame.mDisp1_digit2);
+        mDisplay1_third_digit.setText(mGame.mDisp1_digit3);
+
+        mDisplay2_first_digit.setText(mGame.mDisp2_digit1);
+        mDisplay2_second_digit.setText(mGame.mDisp2_digit2);
+        mDisplay2_third_digit.setText(mGame.mDisp2_digit3);
+
+    }
+
+    // Dialog box to confirm exit from the application
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setMessage(R.string.azsoftware_exit_question_string)
+                .setCancelable(false)
+                .setPositiveButton(R.string.azsoftware_yes_string, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        MainActivity.this.finish();
+                    }
+                })
+                .setNegativeButton(R.string.azsoftware_no_string, null)
+                .show();
+    }
+
 }
